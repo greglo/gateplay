ed26063dacf59f103a241a2c6203bd10cea17f8b
Greg O'Connor <gregory@oconnor.name>
2014-04-28 02:38:11 +0100

    WireView uses Polyline rather than multiple Lines

--- js/canvas/views/wireview.js
+++ js/canvas/views/wireview.js
@@ -1,10 +1,14 @@
 define([
+    "underscore",
     "backbone",
     "fabric",
-], function(Backbone, fabric) {
+    "sim/truthvalue",
+], function(_, Backbone, fabric, TruthValue) {
     return Backbone.View.extend({
         initialize: function(options) {
             this.options = options.options;
+
+            this.model.on("change:truthValue", this._setWireColor, this);
         },
 
         render : function() {
@@ -13,87 +17,66 @@ define([
             var components = this.options.components;
 
             var fixedPoints = model.get("fixedPoints");
-                
+            var canvasFixedPoints = _.map(fixedPoints, function(point) {
+                return {
+                    x: point.x * GRID_SIZE,
+                    y: point.y * GRID_SIZE
+                }
+            });
+
             var sourceComponent = components.models[(model.get("sourceId"))];
             var firstX = sourceComponent.get("x") + sourceComponent.get("width");
             var firstY = sourceComponent.getOutputCoordinate(model.get("sourcePort"));
+            canvasFixedPoints.unshift({
+                x: (firstX - 0.5) * GRID_SIZE,
+                y: firstY * GRID_SIZE 
+            });
             
-            var lastPoint = {x: firstX, y: firstY};
-
-            for (var i = 0; i < fixedPoints.length; i++) {
-                var thisPoint = fixedPoints[i];
-                this._renderSegment(lastPoint.x, lastPoint.y, thisPoint.x, thisPoint.y)
-                lastPoint = thisPoint;
-            }
 
             var targetComponent = components.models[(model.get("targetId"))];
             var lastX = targetComponent.get("x");
             var lastY = targetComponent.getInputCoordinate(model.get("targetPort"));
+            canvasFixedPoints.push({
+                x: (lastX - 0.5) * GRID_SIZE,
+                y: lastY * GRID_SIZE 
+            });
 
-            this._renderSegment(lastPoint.x, lastPoint.y, lastX, lastY);
+
+            var xs = _.pluck(canvasFixedPoints, "x");
+            var smallestX = _.reduce(xs, function(x, min) {
+                return Math.min(x, min);
+            });
+
+            var ys = _.pluck(canvasFixedPoints, "y");
+            var smallestY = _.reduce(ys, function(y, min) {
+                return Math.min(y, min);
+            });
+
+            this.polyLine = new fabric.Polyline(canvasFixedPoints, {
+              strokeWidth: GRID_SIZE,
+              fill: false,
+              left: smallestX,
+              top: smallestY,
+              selectable: false,
+              rx: GRID_SIZE,
+              ry: GRID_SIZE,
+              evented: false
+            });
+            this._setWireColor();
+
+            this.options.canvas.add(this.polyLine);
         },
 
-        _renderSegment: function(x1, y1, x2, y2) {
-            // A segment must be either horizontal or vertical
-            if (x1 === x2 || y1 === y2) {
-                
-                var GRID_SIZE = this.options.GRID_SIZE;
-
-                if (x1 !== x2) {
-                    var y = y1;
-
-                    var xOffset = 0;
-                    if (x1 > x2) {
-                        var temp = x2;
-                        x2 = x1;
-                        x1 = temp;
-                        xOffset = GRID_SIZE;
-                    }
-
-                    var a = x1 * GRID_SIZE + xOffset;
-                    var b = x2 * GRID_SIZE + xOffset;
-                    var c = y * GRID_SIZE;
-
-                    var segment = new fabric.Line([a, c, b, c], {
-                        strokeWidth: GRID_SIZE,
-                        stroke: "black",
-                        originX: "center",
-                        originY: "top",
-                        selectable: false
-                    });
-                    this.options.canvas.add(segment);
-                } else if (y1 !== y2) {
-                    var x = x1;
-
-                    var yOffset = 0;
-                    if (y1 > y2) {
-                        var temp = y2;
-                        y2 = y1;
-                        y1 = temp;
-                        yOffset = GRID_SIZE;
-                    }
-
-                    var a = y1 * GRID_SIZE + yOffset;
-                    var b = y2 * GRID_SIZE + yOffset;
-                    var c = x * GRID_SIZE;
-
-                    var segment = new fabric.Line([c, a, c, b], {
-                        strokeWidth: GRID_SIZE,
-                        stroke: "black",
-                        originX: "left",
-                        originY: "center",
-                        selectable: false
-                    });
-                    this.options.canvas.add(segment);
-                }
-
-            } else {
-                console.log(x1);
-                console.log(y1);
-                console.log(x2);
-                console.log(y2);
-                console.warn("Tried to draw a non-axis-aligned wire segment")
+        _setWireColor: function() {
+            var truthValue = this.model.get("truthValue");
+            console.log(truthValue);
+            if (truthValue === TruthValue.TRUE) {
+                this.polyLine.setStroke("Green");;
+            } else if (truthValue === TruthValue.FALSE) {
+                this.polyLine.setStroke("Red");
+            } else if (truthValue === TruthValue.UNKNOWN) {
+                this.polyLine.setStroke("Gray");
             }
-        },
+        }
     });
 });
